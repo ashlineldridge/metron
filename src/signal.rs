1a9bafd864d7e373fa57a623c33d83441061df31
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2022-03-14 06:43:33 +1000

    Refactored signal.rs

--- src/signal.rs
+++ src/signal.rs
@@ -2,13 +2,10 @@ use crate::schedule::RequestSchedule;
 
 use anyhow::Error;
 use std::time::Instant;
-use tokio::{sync::mpsc::Receiver, task::JoinHandle};
+use tokio::{sync::mpsc::Sender, task::JoinHandle};
 
-// Make it so it's possible to combine multiple signal generators
-// e.g., to interleave requests
 pub struct SignalGenerator {
-    pub handle: JoinHandle<Result<(), Error>>,
-    pub stream: Receiver<Signal>,
+    tx: Sender<Signal>,
 }
 
 #[derive(Debug)]
@@ -16,43 +13,43 @@ pub struct Signal {
     pub due: Instant,
 }
 
-pub fn blocking_generator<S>(schedule: S, chan_size: usize) -> SignalGenerator
-where
-    S: Iterator<Item = RequestSchedule> + Send + 'static,
-{
-    let (tx, rx) = tokio::sync::mpsc::channel(chan_size);
+impl SignalGenerator {
+    fn new(tx: Sender<Signal>) -> Self {
+        Self { tx }
+    }
 
-    let handle = tokio::task::spawn_blocking(move || {
-        for r in schedule {
-            wait_until(r.start);
-            tx.blocking_send(Signal { due: r.start })?;
-        }
+    pub fn run_blocking<S>(&self, schedule: S) -> JoinHandle<Result<(), Error>>
+    where
+        S: Iterator<Item = RequestSchedule> + Send + 'static,
+    {
+        let tx = self.tx.clone();
+        tokio::task::spawn_blocking(move || {
+            for r in schedule {
+                spin_until(r.start);
+                tx.blocking_send(Signal { due: r.start })?;
+            }
 
-        Ok(())
-    });
+            Ok(())
+        })
+    }
 
-    SignalGenerator { handle, stream: rx }
+    pub fn run_async<S>(&self, schedule: S) -> JoinHandle<Result<(), Error>>
+    where
+        S: Iterator<Item = RequestSchedule> + Send + 'static,
+    {
+        let tx = self.tx.clone();
+        tokio::task::spawn(async move {
+            for r in schedule {
+                sleep_until(r.start);
+                tx.send(Signal { due: r.start }).await?;
+            }
+
+            Ok(())
+        })
+    }
 }
 
-pub fn async_generator<S>(schedule: S, chan_size: usize) -> SignalGenerator
-where
-    S: Iterator<Item = RequestSchedule> + Send + 'static,
-{
-    let (tx, rx) = tokio::sync::mpsc::channel(chan_size);
-
-    let handle = tokio::task::spawn(async move {
-        for r in schedule {
-            wait_until(r.start);
-            tx.send(Signal { due: r.start }).await?;
-        }
-
-        Ok(())
-    });
-
-    SignalGenerator { handle, stream: rx }
-}
-
-fn wait_until(t: Instant) {
+fn spin_until(t: Instant) {
     loop {
         if Instant::now() >= t {
             break;
@@ -60,4 +57,8 @@ fn wait_until(t: Instant) {
 
         std::hint::spin_loop();
     }
+}
+
+async fn sleep_until(t: Instant) {
+    tokio::time::sleep_until(t.into()).await
 }
