3baafedc76f3240d98f7bb92ae48a21ff06e75d5
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2022-06-06 09:49:22 +1000

    Removed prometheus crate in favor of metrics crate.

--- src/profile/metrics.rs
+++ src/profile/metrics.rs
@@ -1,7 +1,4 @@
-use std::collections::HashMap;
-
 use anyhow::Result;
-use prometheus::{proto::MetricFamily, BasicAuthentication};
 
 use super::profiler::Sample;
 
@@ -47,97 +44,97 @@ impl Backend {
     // }
 }
 
-#[allow(dead_code)]
-const LABEL_NAME_JOB: &str = "job";
+// #[allow(dead_code)]
+// const LABEL_NAME_JOB: &str = "job";
 
-#[allow(dead_code)]
-async fn push(
-    _job: &str,
-    _grouping: HashMap<String, String>,
-    _url: &str,
-    _mfs: Vec<MetricFamily>,
-    _method: &str,
-    _basic_auth: Option<BasicAuthentication>,
-) -> Result<()> {
-    // Suppress clippy warning needless_pass_by_value.
-    // let grouping = grouping;
+// #[allow(dead_code)]
+// async fn push(
+//     _job: &str,
+//     _grouping: HashMap<String, String>,
+//     _url: &str,
+//     _mfs: Vec<MetricFamily>,
+//     _method: &str,
+//     _basic_auth: Option<BasicAuthentication>,
+// ) -> Result<()> {
+//     // Suppress clippy warning needless_pass_by_value.
+//     // let grouping = grouping;
 
-    // let mut push_url = if url.contains("://") {
-    //     url.to_owned()
-    // } else {
-    //     format!("http://{}", url)
-    // };
+//     // let mut push_url = if url.contains("://") {
+//     //     url.to_owned()
+//     // } else {
+//     //     format!("http://{}", url)
+//     // };
 
-    // if push_url.ends_with('/') {
-    //     push_url.pop();
-    // }
+//     // if push_url.ends_with('/') {
+//     //     push_url.pop();
+//     // }
 
-    // let mut url_components = Vec::new();
-    // if job.contains('/') {
-    //     bail!("job contains '/': {}", job);
-    // }
+//     // let mut url_components = Vec::new();
+//     // if job.contains('/') {
+//     //     bail!("job contains '/': {}", job);
+//     // }
 
-    // // TODO: escape job
-    // url_components.push(job.to_owned());
+//     // // TODO: escape job
+//     // url_components.push(job.to_owned());
 
-    // for (ln, lv) in &grouping {
-    //     // TODO: check label name
-    //     if lv.contains('/') {
-    //         bail!("value of grouping label {} contains '/': {}", ln, lv);
-    //     }
-    //     url_components.push(ln.to_owned());
-    //     url_components.push(lv.to_owned());
-    // }
+//     // for (ln, lv) in &grouping {
+//     //     // TODO: check label name
+//     //     if lv.contains('/') {
+//     //         bail!("value of grouping label {} contains '/': {}", ln, lv);
+//     //     }
+//     //     url_components.push(ln.to_owned());
+//     //     url_components.push(lv.to_owned());
+//     // }
 
-    // push_url = format!("{}/metrics/job/{}", push_url, url_components.join("/"));
+//     // push_url = format!("{}/metrics/job/{}", push_url, url_components.join("/"));
 
-    // let encoder = TextEncoder::new();
-    // let mut buf = Vec::new();
+//     // let encoder = TextEncoder::new();
+//     // let mut buf = Vec::new();
 
-    // for mf in mfs {
-    //     // Check for pre-existing grouping labels:
-    //     for m in mf.get_metric() {
-    //         for lp in m.get_label() {
-    //             if lp.get_name() == LABEL_NAME_JOB {
-    //                 bail!(
-    //                     "pushed metric {} already contains a job label",
-    //                     mf.get_name()
-    //                 );
-    //             }
-    //             if grouping.contains_key(lp.get_name()) {
-    //                 bail!(
-    //                     "pushed metric {} already contains grouping label {}",
-    //                     mf.get_name(),
-    //                     lp.get_name()
-    //                 );
-    //             }
-    //         }
-    //     }
-    //     // Ignore error, `no metrics` and `no name`.
-    //     let _ = encoder.encode(&[mf], &mut buf);
-    // }
+//     // for mf in mfs {
+//     //     // Check for pre-existing grouping labels:
+//     //     for m in mf.get_metric() {
+//     //         for lp in m.get_label() {
+//     //             if lp.get_name() == LABEL_NAME_JOB {
+//     //                 bail!(
+//     //                     "pushed metric {} already contains a job label",
+//     //                     mf.get_name()
+//     //                 );
+//     //             }
+//     //             if grouping.contains_key(lp.get_name()) {
+//     //                 bail!(
+//     //                     "pushed metric {} already contains grouping label {}",
+//     //                     mf.get_name(),
+//     //                     lp.get_name()
+//     //                 );
+//     //             }
+//     //         }
+//     //     }
+//     //     // Ignore error, `no metrics` and `no name`.
+//     //     let _ = encoder.encode(&[mf], &mut buf);
+//     // }
 
-    // let https = HttpsConnector::new();
-    // let client = Client::builder().build::<_, hyper::Body>(https);
+//     // let https = HttpsConnector::new();
+//     // let client = Client::builder().build::<_, hyper::Body>(https);
 
-    // let target_uri = push_url.parse::<hyper::Uri>()?;
-    // let req = hyper::Request::builder()
-    //     .method(method)
-    //     .uri(target_uri)
-    //     .header("Content-Type", encoder.format_type())
-    //     .body(hyper::Body::from(buf))?;
+//     // let target_uri = push_url.parse::<hyper::Uri>()?;
+//     // let req = hyper::Request::builder()
+//     //     .method(method)
+//     //     .uri(target_uri)
+//     //     .header("Content-Type", encoder.format_type())
+//     //     .body(hyper::Body::from(buf))?;
 
-    // let resp = client.request(req).await?;
+//     // let resp = client.request(req).await?;
 
-    // match resp.status() {
-    //     StatusCode::OK => Ok(()),
-    //     StatusCode::ACCEPTED => Ok(()),
-    //     _ => bail!(
-    //         "unexpected status code {} while pushing to {}",
-    //         resp.status(),
-    //         push_url
-    //     ),
-    // }
+//     // match resp.status() {
+//     //     StatusCode::OK => Ok(()),
+//     //     StatusCode::ACCEPTED => Ok(()),
+//     //     _ => bail!(
+//     //         "unexpected status code {} while pushing to {}",
+//     //         resp.status(),
+//     //         push_url
+//     //     ),
+//     // }
 
-    Ok(())
-}
+//     Ok(())
+// }
