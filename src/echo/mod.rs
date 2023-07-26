de8a526a3b89ec174a759ee4c1c2987d954c1e3a
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2023-07-26 23:00:03 +1000

    Added some basic http handlers to echo server

--- src/echo/mod.rs
+++ src/echo/mod.rs
@@ -1,15 +1,18 @@
 mod config;
 mod prom;
 
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 
 use anyhow::Result;
 use axum::{
-    http::{StatusCode, Uri},
+    extract::{ConnectInfo, Path},
+    headers::UserAgent,
+    http::{header::HeaderMap, StatusCode, Uri},
     routing::{delete, get, patch, post, put},
-    Router,
+    Json, Router, TypedHeader,
 };
 use log::info;
+use serde_json::{json, Value};
 use tokio::signal;
 
 pub use self::config::Config;
@@ -20,13 +23,13 @@ pub async fn serve(_config: &Config) -> Result<()> {
         .fallback(not_found)
         .route("/", get(get_root))
         .route("/ip", get(get_ip))
-        .route("/uuid", get(get_uuid))
         .route("/headers", get(get_headers))
         .route("/get", get(get_headers))
         .route("/post", post(get_headers))
         .route("/patch", patch(get_headers))
         .route("/put", put(get_headers))
         .route("/delete", delete(get_headers))
+        .route("/user-agent", get(get_user_agent))
         .route("/status/:code", get(get_status))
         .route("/delay/:duration", get(get_delay));
 
@@ -34,7 +37,7 @@ pub async fn serve(_config: &Config) -> Result<()> {
     info!("listening on {}", addr);
 
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .with_graceful_shutdown(shutdown())
         .await?;
 
@@ -51,26 +54,34 @@ async fn not_found(uri: Uri) -> (StatusCode, String) {
 }
 
 async fn get_root() -> (StatusCode, String) {
-    (StatusCode::OK, "Hello\n".to_owned())
+    (StatusCode::OK, "Hello".to_owned())
 }
 
-async fn get_ip() -> (StatusCode, String) {
-    (StatusCode::OK, "Hello\n".to_owned())
+async fn get_ip(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> (StatusCode, Json<Value>) {
+    (StatusCode::OK, Json(json!({ "origin": addr.ip() })))
 }
 
-async fn get_uuid() -> (StatusCode, String) {
-    (StatusCode::OK, "Hello\n".to_owned())
+async fn get_headers(headers: HeaderMap) -> (StatusCode, Json<Value>) {
+    let mut m = HashMap::new();
+    for (k, v) in headers.iter() {
+        if let Ok(v) = v.to_str() {
+            m.insert(k.to_string(), v.to_string());
+        }
+    }
+
+    (StatusCode::OK, Json(json!({ "headers": m })))
 }
 
-async fn get_headers() -> (StatusCode, String) {
-    (StatusCode::OK, "Hello\n".to_owned())
+async fn get_user_agent(TypedHeader(user_agent): TypedHeader<UserAgent>) -> Json<Value> {
+    Json(json!({ "user-agent": user_agent.to_string() }))
 }
 
-async fn get_status() -> (StatusCode, String) {
-    (StatusCode::OK, "Hello\n".to_owned())
+async fn get_status(Path(status): Path<u16>) -> StatusCode {
+    StatusCode::from_u16(status).unwrap_or(StatusCode::BAD_REQUEST)
 }
 
-async fn get_delay() -> (StatusCode, String) {
+async fn get_delay(Path(_delay): Path<String>) -> (StatusCode, String) {
+    // TODO next
     (StatusCode::OK, "Hello\n".to_owned())
 }
 
