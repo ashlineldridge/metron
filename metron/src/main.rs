a75fc06e92b467aaff003566a184d572d5433c34
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2023-08-27 09:36:36 +1000

    Simple proto

--- metron/src/main.rs
+++ metron/src/main.rs
@@ -1,3 +1,10 @@
+use proto::agent::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let agent_config = Config {
+        port: 100,
+        name: "foobar".to_owned(),
+    };
+
+    println!("Hello, world! {:?}", agent_config);
 }
